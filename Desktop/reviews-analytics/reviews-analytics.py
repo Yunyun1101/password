589ed20c2da9b1555f6c641bec5bae3163ad863d
Vicky Yun <marsyunyun1101@gmail.com>
2019-06-12 17:49:51 +0800

    add word count function

---
+++ Desktop/reviews-analytics/reviews-analytics.py
@@ -0,0 +1,72 @@
+data = []
+count = 0
+length = []
+line_length = 0
+
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		length.append(len(line))
+		line_length = line_length + len(line)
+		count += 1
+
+average = line_length / count
+
+print('檔案讀取完了, 總共有',len(data), '筆資料') 
+print('留言平均長度為', average)
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+
+print('一共有', len(new), '筆長度小於100的資料')
+print(new[0])
+print(new[1])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
+
+# 快寫法
+good = [1 for d in data if 'good' in d]
+print(good) 
+
+# 快寫法 list comprehension
+bad = ['bad' in d for d in data]
+print(bad)reviews-analytics
+
+#文字計數
+wc = {} # word_count
+for d in data:
+	words = d.split() #預設值是空字串
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 100000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word = input('請問你要查什麼字？')
+	if word  == 'q':
+		print('感謝使用本功能')
+		break
+	if word in wc:
+		print(word, '出現過', wc[word], '次')
+	else:
+		print(word, '不存在此字典')
+
+
